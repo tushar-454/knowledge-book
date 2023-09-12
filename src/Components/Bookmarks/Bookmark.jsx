191e32401362221032e347972bb9bf980788f788
tushar-454 <imtushar454@gmail.com>
2023-09-12 12:37:57 +0600

    bookmark section style completed with dynamic data get

---
+++ src/Components/Bookmarks/Bookmark.jsx
@@ -0,0 +1,11 @@
+const Bookmark = () => {
+  return (
+    <div className='bg-white mb-4 p-5 rounded-lg'>
+      <h1 className='text-xl font-semibold leading-[28px]'>
+        Master Microsoft Power Platform and Become an In-Demand!
+      </h1>
+    </div>
+  );
+};
+
+export default Bookmark;
--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,7 +1,22 @@
+import Bookmark from './Bookmark';
+
 const Bookmarks = () => {
+  const arr = [1, 2, 3, 4];
   return (
     <div className='w-full lg:w-1/3 py-8'>
-      <h1 className='text-5xl'>this is a bookmarks section</h1>
+      <div className='border border-[#6047EC] bg-[#6047EC1A] rounded-lg'>
+        <h1 className='text-2xl text-[#6047EC] font-bold leading-[38px] py-5 px-10'>
+          Spent time on read : 177 min
+        </h1>
+      </div>
+      <div className='allSingleBookmark mt-6 p-6 bg-[#1111110D] rounded-lg'>
+        <h1 className='text-2xl font-bold leading-[38px] mb-4'>
+          Bookmarked Blog: 8
+        </h1>
+        {arr.map((bookmark, index) => (
+          <Bookmark key={index} />
+        ))}
+      </div>
     </div>
   );
 };
