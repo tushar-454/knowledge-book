8a89146dd28791e39432ba8f6810d9a1a6bd89cd
tushar-454 <imtushar454@gmail.com>
2023-09-12 11:06:50 +0600

    knowledge book header section completed

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import headLogo from '../../assets/images/profile.png';
+
+const Header = () => {
+  return (
+    <div className='w-9/12 mx-auto'>
+      <div className='headerWrap flex justify-between items-center py-8 border-b border-[#11111126]'>
+        <h1 className='text-5xl font-bold leading-[64px]'>Knowledge Cafe</h1>
+        <img src={headLogo} alt='headLogo' className='w-14' />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
